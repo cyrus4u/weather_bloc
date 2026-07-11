faf5c5a8f48c5cbcb6532d6908b64432fc9a0e3f
cyrus4u <cyrus4u@users.noreply.github.com>
2026-07-11 10:33:16 +0330

    update

--- lib/core/widgets/main_wrapper.dart
+++ lib/core/widgets/main_wrapper.dart
@@ -1,10 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:weather_bloc/core/widgets/app_background.dart';
 import 'package:weather_bloc/core/widgets/bottom_nav.dart';
 import 'package:weather_bloc/features/feature_bookmark/presentation/screens/bookmark_screen.dart';
-import 'package:weather_bloc/features/feature_weather/presentation/bloc/bloc/home_bloc.dart';
 import 'package:weather_bloc/features/feature_weather/presentation/screens/home_screen.dart';
+import 'package:weather_bloc/locator.dart';
 
 class MainWrapper extends StatelessWidget {
   MainWrapper({super.key});
@@ -14,7 +13,7 @@ class MainWrapper extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     List<Widget> pageViewWidget = [
-      HomeScreen(),
+      HomeScreen(getSuggestionCityUsecase: locator()),
       BookmarkScreen(pageController: pageController),
     ];
 
